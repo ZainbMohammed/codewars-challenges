9aedd11048d10ef2bf6ac32094b52cf8acb25594
Zinam Mohamed <pk.x3311@gmail.com>
2024-05-14 03:05:25 +0300

    solve 2 challengase

--- chalenges.js
+++ chalenges.js
@@ -206,6 +206,51 @@ console.log(binaryArrayToNumber([1, 0, 1, 1]));  // Output: 11
 
 // ========= another slotion ==========
 
-const binaryArrayToNumber = arr => {
-  return arr.reduce((total, cur) => (total = total * 2 + cur), 0);
+// const binaryArrayToNumber = arr => {
+//   return arr.reduce((total, cur) => (total = total * 2 + cur), 0);
+// }
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+*/
+
+function sumMultiples(number) {
+  if (number <= 0) {
+      return 0;
+  }
+
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+          sum += i;
+      }
+  }
+
+  return sum;
 }
+
+console.log('======= sumMultiples =======');
+console.log('10 => ' + sumMultiples(10));
+console.log('11 => ' + sumMultiples(11));
+console.log('15 => ' + sumMultiples(15));
+console.log('21 => ' + sumMultiples(21));
+console.log('-21 => ' + sumMultiples(-21));
+
+// ================= another slotion =========
+function solution(number){
+  return number < 1 ? 0 : [...new Array(number).keys()].filter(n => n % 3 == 0 || n % 5 == 0).reduce((a, b) => a + b);
+}
+
+/*
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+*/
